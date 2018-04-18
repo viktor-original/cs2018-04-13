59766c9db058a3d5326cebf01a9941cf187c3978
Viktor <viktor.originaly@gmail.com>
2018-04-18 12:21:30 +0300

    Lesson2 First Commit

---
+++ src/by/it/krasilnikov/lesson02/TaskA2.java
@@ -0,0 +1,16 @@
+package by.it.krasilnikov.lesson02;
+
+/*
+напишите тело main программы которая выводит на экран 5 строк
+"Я начинаю изучать Java!"
+
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+        }
+    }
+}
