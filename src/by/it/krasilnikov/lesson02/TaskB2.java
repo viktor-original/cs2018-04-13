5f7aac05bfcef99c27faaf5f7859b79cf4b7121d
Viktor <viktor.originaly@gmail.com>
2018-04-19 19:30:38 +0300

    Task B Completed

--- src/by/it/krasilnikov/lesson02/TaskB2.java
+++ src/by/it/krasilnikov/lesson02/TaskB2.java
@@ -22,7 +22,7 @@ class TaskB2 {
 
     public static void main(String[] args) {
 
-        int result = + a + b + c + d;
+        int result = - a + b - c + d;
 
         System.out.println(result);
     }
--- src/by/it/krasilnikov/lesson02/TaskB3.java
+++ src/by/it/krasilnikov/lesson02/TaskB3.java
@@ -10,15 +10,13 @@ package by.it.krasilnikov.lesson02;
 */        
 class TaskB3 {
         public static void main(String[] args) {
-//        String s = "C";
-//        System.out.println("Новым Годом");
 //        System.out.println("C Новым");
 //        System.out.println("Годом");
-//        System.out.print("C Новым");
+        System.out.print("C Новым");
 //        System.out.println("Годом");
 //        System.out.println(s);
 //        System.out.print("C Новым!");
-//        System.out.print(" ");
-//        System.out.println("Годом");
+        System.out.print(" ");
+        System.out.println("Годом");
     }
 }
