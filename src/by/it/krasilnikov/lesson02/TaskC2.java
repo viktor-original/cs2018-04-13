478b82418c105d122e3b28e00656509817a5ce3a
Viktor <viktor.originaly@gmail.com>
2018-04-20 11:28:03 +0300

    Lesson 3 Start

--- src/by/it/krasilnikov/lesson02/TaskC2.java
+++ src/by/it/krasilnikov/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.krasilnikov.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -14,6 +16,14 @@ HEX:22+1a=3c
 OCT:42+32=74
 */
 class TaskC2 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int sum = b + c;
+        int sumb = 0bsum;
+        System.out.println("DEC:" + b + c + "=" + sum);
+        System.out.println("BIN:" + 0bb + 0bc + "=" + sumb );
+    }
 
 }
